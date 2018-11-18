85ee803eba7b3b3bc84c1ea0de812b00ba6a8837
Jillian Jarrett <jillian.e.jarrett@gmail.com>
2018-11-18 14:34:09 -0500

    pre checkpoint work: completed_v2

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 window.onload = function() {
    alert("The window has loaded!");
+   onReady();
  };
+
+ function onReady(){
+   const addToDoForm = documnet.getElementById(('addToDoForm'));
+   const newToDoText = documnet.getElementById('newToDoText');
+   const toDoList = documnet.getElementById('toDoList')
+   addToDoForm.addEventListener('submit', event =>{
+     event.preventDefault();
+     let title = newToDoText.value;
+     let newLi = documnet.createElement('li')
+     let checkbox = documnet.createElement('input');
+     checkbox.type = "checkbox";
+
+     newLi.textContent = title;
+
+     newLi.appendChild(checkbox);
+     toDoList.appendChild(newLi);
+     newToDoText.value='';
+
+   });
+ }
