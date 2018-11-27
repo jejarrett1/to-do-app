43b2865630722c2f650e370d4247893460c4631e
Jillian Jarrett <jillian.e.jarrett@gmail.com>
2018-11-27 07:49:41 -0500

    assignment complete

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 function onReady(){
-  const toDos = [];
+  var toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  let id=0;
 
+  tempToDos = localStorage["toDos"]
+  console.log(tempToDos);
+  if (tempToDos){
+    console.log("hey");
+    toDos = JSON.parse(tempToDos);
+    console.log(toDos);
+  }
   function createNewToDo() {
+    id=id+1;
     const newToDoText = document.getElementById('newToDoText');
 
     if (!newToDoText.value) { return; }
 
     toDos.push({
       title: newToDoText.value,
-      complete: false
+      complete: false,
+      id: id.value
     });
 
     newToDoText.value = '';
 
     renderTheUI();
-    
+    var temp = JSON.stringify(toDos);
+    localStorage["toDos"]=temp;
   }
   function renderTheUI() {
     const toDoList = document.getElementById('toDoList');
@@ -26,18 +37,51 @@ function onReady(){
       const newLi = document.createElement('li');
       const checkbox = document.createElement('input');
       checkbox.type = "checkbox";
+      const deleteButton =document.createElement('button');
+      deleteButton.textContent="Delete";
+      deleteButton.className="mdl-button mdl-js-button mdl-button--accent";
 
       newLi.textContent = toDo.title;
 
-      toDoList.appendChild(newLi);
+
       newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+      deleteButton.addEventListener('click', event=>{
+        deleteToDo(toDo);
+      });
+      checkbox.addEventListener('click', event=>{
+        deleteToDo(toDo);
+      });
+
+
+      toDoList.appendChild(newLi);
     });
 
+
+  }
+  function completeToDo(toDo){
+    toDos = toDos.filter( (todo) => {
+      if (todo==toDo){
+        toDo.complete=True;
+      }
+    });
+
+  }
+
+
+  function deleteToDo(toDo){
+    toDos = toDos.filter( (todo) => {
+      return todo != toDo;
+    });
+    renderTheUI(toDos);
+    // var temp = JSON.stringify(toDos);
+    // localStorage.setItem("toDos",temp);
   }
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
   });
+
   renderTheUI();
 }
 
