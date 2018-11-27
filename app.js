844cd47e03ee01243fe1b6fe594d87931262206c
Jillian Jarrett <jillian.e.jarrett@gmail.com>
2018-11-26 19:26:37 -0500

    checkpoint work complete

--- app.js
+++ app.js
@@ -1,54 +1,50 @@
 function onReady(){
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+
+    newToDoText.value = '';
+
+    renderTheUI();
+    
+  }
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+
+  }
   addToDoForm.addEventListener('submit', event => {
-  event.preventDefault();
-
-  // get the text
-  let title = newToDoText.value;
-
-  // create a new li
-  let newLi = document.createElement('li');
-
-  // create a new input
-  let checkbox = document.createElement('input');
-  let deleteButton =document.createElement('button');
-  deleteButton.textContent="Delete";
-  deleteButton.className="mdl-button mdl-js-button mdl-button--accent"
-
-  // set the input's type to checkbox
-  checkbox.type = "checkbox";
-
-
-  // set the title
-  newLi.textContent = title;
-
-  // attach the checkbox to the li
-  newLi.appendChild(checkbox);
-  newLi.appendChild(deleteButton);
-  deleteButton.addEventListener('click', event=>{
-    console.log("herd click");
-    deleteToDo(newLi);
+    event.preventDefault();
+    createNewToDo();
   });
-
-  // attach the li to the ul
-  toDoList.appendChild(newLi);
-
-  //empty the input
-  newToDoText.value = '';
-  });
-
-  function deleteToDo(Li) {
-    console.log("in delete funtions");
-    console.log(Li)
-    let todo = document.getElementsByTagName("ul")[0];
-    console.log(todo);
-    toDoList.removeChild(Li);
-};
+  renderTheUI();
 }
+
+
+
 
 window.onload = function() {
   onReady();
-  console.log("hey")
+
 };
